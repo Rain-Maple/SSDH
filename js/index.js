c5cccf5ae8b8734d6af4a76d17c9bc8adcace11b
Rain-Maple <83900858+Rain-Maple@users.noreply.github.com>
2025-04-28 11:21:34 +0800

    更新 index.js

--- js/index.js
+++ js/index.js
@@ -3,43 +3,43 @@ const searchEngines = [
   {
     slogan: "必应",
     action: "https://www.bing.com/search",
-    /paramKey: "q",
+    /paramKey: "q"
     //hidden: { name: "", value: "n" }
   },
   {
     slogan: "百度一下，你就知道",
     action: "http://www.baidu.com/s",
-    paramKey: "wd",
+    paramKey: "wd"
     //hidden: { name: "ie", value: "utf-8" }
   },
   {
     slogan: "Google",
     action: "https://www.google.com/search",
-    paramKey: "query",
+    paramKey: "query"
     //hidden: { name: "qs", value: "n" }
   },
   {
     slogan: "360搜索，SO靠谱",
     action: "https://www.so.com/s",
-    paramKey: "q",
+    paramKey: "q"
     //hidden: { name: "ie", value: "utf-8" }
   },
   {
     slogan: "知乎",
     action: "https://www.zhihu.com/search",
-    paramKey: "q",
+    paramKey: "q"
     //hidden: { name: "", value: "" }
   },
   {
     slogan: "搜狗搜索引擎 - 上网从搜狗开始",
     action: "https://www.sogou.com/web",
-    paramKey: "query",
+    paramKey: "query"
     //hidden: { name: "ie", value: "utf8" }
   },
 		{
     slogan: "今日头条",
     action: "https://www.toutiao.com/search",
-    paramKey: "keyword",
+    paramKey: "keyword"
     //hidden: { name: "ie", value: "utf8" }
   }
 ];
